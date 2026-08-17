983011525f4d5f7db9301b0ec5f117210ff64e17
MCxlm1 <115008407+MCxlm1@users.noreply.github.com>
2026-08-17 12:33:02 +0800

    翻译: AimAssistTargetMode.d.ts

---
+++ translations/zh-CN/preview-beta/server/enums/AimAssistTargetMode.d.ts
@@ -0,0 +1,17 @@
+/**
+ * 指定用于瞄准辅助的不同目标模式。
+ */
+export enum AimAssistTargetMode {
+    /**
+     * @remarks
+     * 基于角度的瞄准。
+     *
+     */
+    Angle = 'Angle',
+    /**
+     * @remarks
+     * 基于距离的瞄准。
+     *
+     */
+    Distance = 'Distance',
+}
