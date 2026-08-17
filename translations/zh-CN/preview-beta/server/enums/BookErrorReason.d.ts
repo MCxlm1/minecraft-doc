d3a2e8b68229dede087e37808e6203d5fe0727b9
MCxlm1 <115008407+MCxlm1@users.noreply.github.com>
2026-08-17 12:33:08 +0800

    翻译: BookErrorReason.d.ts

---
+++ translations/zh-CN/preview-beta/server/enums/BookErrorReason.d.ts
@@ -0,0 +1,23 @@
+/**
+ * 一个与使用 {@link ItemBookComponent} 相关的错误原因枚举。
+ */
+export enum BookErrorReason {
+    /**
+     * @remarks
+     * 请求的页面内容超过了最大页面长度 256。
+     *
+     */
+    ExceedsMaxPageLength = 'ExceedsMaxPageLength',
+    /**
+     * @remarks
+     * 无法创建该页面，因为它会超过最大页数 50。
+     *
+     */
+    ExceedsMaxPages = 'ExceedsMaxPages',
+    /**
+     * @remarks
+     * 正在签名的标题超过了最大标题长度 16。
+     *
+     */
+    ExceedsTitleLength = 'ExceedsTitleLength',
+}
